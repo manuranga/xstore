1f115e02e7250f1bae83e9ca7235b00de73c4e02
Manu Perera <manu@wso2.com>
2014-07-22 17:30:05 +0530

    init commit

---
+++ units/VerticalMenu/controller.js
@@ -0,0 +1,10 @@
+/*
+var isLoged = (typeof(userName) != 'undefined' && userName != null && userName!="")
+var displayName;
+
+if (isLoged){
+    displayName= "Hello, " + userName;
+}else{
+    displayName= "Guest";
+}
+*/

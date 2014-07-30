ef64b428c55c697eb2cc393ff41d0acefa3ed8fc
Manu Perera <manu@wso2.com>
2014-07-30 14:05:34 +0530

    adding asset extensions

---
+++ extensions/assets/default/asset.js
@@ -0,0 +1,14 @@
+asset.manager = function (ctx) {
+    return {
+    };
+};
+asset.server = function (ctx) {
+    return {
+    };
+};
+asset.configure = function () {
+    return {  };
+};
+asset.renderer = function (ctx) {
+    return {  };
+};
